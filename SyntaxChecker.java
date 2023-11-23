adda62f7d1608d77197457c07686574400782b0d
ZakPug <51068330+ZakPug@users.noreply.github.com>
2023-11-23 03:41:40 +0000

    Initial commit

---
+++ SyntaxChecker.java
@@ -0,0 +1,60 @@
+//Zakwan Berlin
+//Sytax Checker (with web help)
+import java.io.*;
+import java.util.*;
+
+public class SyntaxChecker {
+    public static void main(String[] args) throws IOException {
+        // Check command-line argument
+        if (args.length != 1) {
+            System.out.println("Usage: Java SyntaxChecker Source-codeFileName");
+            System.exit(0);
+        }
+
+        // Check if file exists
+        File file = new File(args[0]);
+        if (!file.exists()) {
+            System.out.println("The file " + args[0] + " does not exist!");
+            System.exit(1);
+        }
+
+        // Create a stack
+        Stack<Character> symbols = new Stack<>();
+
+        try (Scanner input = new Scanner(file)) {
+            // Continuously read chars from input
+            while (input.hasNext()) {
+                String line = input.nextLine();
+                for (int i = 0; i < line.length(); i++) {
+                    char ch = line.charAt(i);
+                    // Push symbols (, {, and [ on to the stack
+                    if (ch == '(' || ch == '{' || ch == '[') {
+                        symbols.push(ch);
+                    } // Process stack
+                    else if (ch == ')' || ch == '}' || ch == ']') {
+                        processSymbols(symbols, ch);
+                    }
+                }
+            }
+        }
+
+        System.out.println("The Java source-code " +
+                (symbols.isEmpty() ? "has" : "does not have") + " correct pairs.");
+    }
+
+    /** Method Matches grouping symbols */
+    private static void processSymbols(Stack<Character> stack, Character ch) {
+        // Remove matching symbols from stack
+        if ((stack.peek() == '(' && ch == ')') ||
+                (stack.peek() == '[' && ch == ']') ||
+                (stack.peek() == '{' && ch == '}')) {
+            stack.pop();
+        } else if ((stack.peek() != '(' && ch == ')') ||
+                (stack.peek() != '[' && ch == ']') ||
+                (stack.peek() != '{' && ch == '}')) {
+            System.out.println("The Java source-code does not have" +
+                    " correct pairs.");
+            System.exit(1);
+        }
+    }
+}
